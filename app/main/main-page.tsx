7891a0c26cee2a0a5b17cf3d4a5329a64c4d1967
Grace Karina <gracekarina12@gmail.com>
2025-10-18 23:20:16 +0700

    feat: add main page

---
+++ app/main/main-page.tsx
@@ -0,0 +1,9 @@
+import CountdownTimer from "components/countdown-timer";
+
+export function MainPage() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <CountdownTimer />
+    </main>
+  );
+}
