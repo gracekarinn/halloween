7696b1a3958dfacee8c9de53ae184c3ae7a0add0
Grace Karina <gracekarina12@gmail.com>
2025-10-19 00:30:43 +0700

    feat: add footer component

---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="w-full border-t border-halloween-3 py-6 text-center text-sm text-halloween-3">
+      Made with &#10084; by HackyUci
+    </footer>
+  );
+}
