049fdc67af49e1943cbc6e915ab3dc6343403b31
Grace Karina <gracekarina12@gmail.com>
2025-10-18 23:20:26 +0700

    feat: update home route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { MainPage } from "~/main/main-page";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "HEX" },
+    { name: "description", content: "Welcome to HEX!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <MainPage />;
 }
