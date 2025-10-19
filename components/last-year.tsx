e1b83864eb8a5a06ef3346f8897b616fa865f7f0
Grace Karina <gracekarina12@gmail.com>
2025-10-19 13:49:15 +0700

    feat: add last year component to display last year's fright

---
+++ components/last-year.tsx
@@ -0,0 +1,17 @@
+export function LastYear() {
+  return (
+    <section className="w-full flex flex-col items-center text-center mt-20 mb-16 text-[#FEFBC1] px-4">
+      <h2 className="text-2xl md:text-4xl font-bold mb-10">
+        Last year&apos;s Fright
+      </h2>
+
+      <div className="w-full max-w-4xl flex justify-center">
+        <img
+          src="/foto-orang.png"
+          alt="Last Year's Fright"
+          className="w-full h-auto object-contain"
+        />
+      </div>
+    </section>
+  );
+}
