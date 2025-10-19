a3cc63be2af19a195b0993e9d6e7244e33650c35
Grace Karina <gracekarina12@gmail.com>
2025-10-19 16:55:11 +0700

    fix: update link

--- components/secure-spot-button.tsx
+++ components/secure-spot-button.tsx
@@ -8,7 +8,11 @@ export function SecureSpotButton() {
         asChild
         className="relative z-10 rounded-[40px] text-2xl border-[4px] border-[#B0B561] bg-halloween-3 text-halloween-1 font-normal px-12 py-8 shadow-[0_0_75px_rgba(252,249,194,0.60)] transition-transform hover:scale-105 hover:shadow-[0_0_90px_rgba(252,249,194,0.80)]"
       >
-        <a href="https://test.com" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://forms.gle/daKeY3GWJaR8wJGF6"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           SECURE YOUR SPOT!
         </a>
       </Button>
