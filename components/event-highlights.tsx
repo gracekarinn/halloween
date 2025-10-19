c8d67dd1b85735d910c6a0e432efced640f74fe4
Grace Karina <gracekarina12@gmail.com>
2025-10-19 14:02:49 +0700

    feat: update event highlights

--- components/event-highlights.tsx
+++ components/event-highlights.tsx
@@ -2,7 +2,7 @@ export function EventHighlights() {
   return (
     <section className="flex flex-row justify-between items-center w-full max-w-3xl mx-auto text-[#FEFBC1] font-bold text-lg md:text-xl mt-12 px-4 gap-x-10">
       <div className="flex flex-col gap-3 md:gap-6 items-start">
-        {["Free Flow", "Best Costumes", "Beats By Loren"].map((item) => (
+        {["Free Flow", "Best Costumes", "DJ Performance"].map((item) => (
           <div key={item} className="flex items-center gap-3">
             <img
               src="/star.png"
